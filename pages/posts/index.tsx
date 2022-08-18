24960d9c8421da690359d11842277d3fcc53ed5f
Đỗ Cao Long <long@myspa.vn>
2022-08-18 11:26:04 +0700

    [UPDATE] fetch

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,40 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 import * as React from 'react'
 
-export interface IAppProps {}
+export interface IPostListPageProps {
+	posts: any[]
+}
 
-export default function App(props: IAppProps) {
-	return <h1>Index Page</h1>
+export default function PostListPage({ posts }: IPostListPageProps) {
+	// log ddc cả bên server và client
+	console.log('posts', posts)
+	return (
+		<>
+			<h1>Post List Page</h1>
+			<ul>
+				{posts.map((item: any, index: number) => (
+					<li key={index}>{item.title}</li>
+				))}
+			</ul>
+		</>
+	)
+}
+
+export interface IPostListPageProps {}
+
+// đc gọi bên server, k gọi ở client
+export const getStaticProps: GetStaticProps<IPostListPageProps> = async (
+	context: GetStaticPropsContext
+) => {
+	// server-side
+	// build-time
+	console.log('first')
+	const response = await fetch('https://js-post-api.herokuapp.com/api/posts?_page=1')
+	const data = await response.json()
+	console.log('data :>> ', data)
+	return {
+		props: {
+			posts: data.data.map((x: any) => ({ id: x.id, title: x.title })),
+		},
+	}
 }
