d4f5372f00d1f9a1cf4014bc7e6454b7c1e85cf8
Đỗ Cao Long <long@myspa.vn>
2022-08-19 15:42:10 +0700

    params-cache

---
+++ pages/posts/params-cache.tsx
@@ -0,0 +1,47 @@
+import { GetServerSidePropsContext } from 'next'
+import { useRouter } from 'next/router'
+import * as React from 'react'
+
+export interface IParamsCacheProps {
+	query: any
+	post: any
+}
+
+export default function ParamsCache({ query, post }: IParamsCacheProps) {
+	console.log('query', query)
+	const router = useRouter()
+	const [seconds, setSeconds] = React.useState()
+	React.useEffect(() => {
+		const intervalId = setInterval(() => {
+			setSeconds((x: any) => {
+				if (x > 60) clearInterval(intervalId)
+				return x + 1
+			})
+		}, 1000)
+	}, [])
+	return (
+		<div>
+			<h1>Params Cache</h1>
+			<p>Time: {seconds}s</p>
+			<h2>Post detail</h2>
+			<p>{post?.title}</p>
+			<p>{post?.author}</p>
+			<p>{post?.description}</p>
+		</div>
+	)
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+	context.res.setHeader('Cache-Control', 's-maxage=5')
+	await new Promise((resolve) => setTimeout(resolve, 3000))
+	const postId = context.query.postId
+	if (!postId) return { props: { query: context.query } }
+	const response = await fetch(`https://js-post-api.herokuapp.com/api/posts?_page=${postId}`)
+	const data = await response.json()
+	return {
+		props: {
+			query: context.query,
+			post: data,
+		},
+	}
+}
